38648f8d3eef12096919d1620c889534da1e284f
Ruben Somuano <rubasomuano.g2@gmail.com>
2024-04-21 20:04:20 -0600

    p

---
+++ PythonOne/01_variablesPython/variables.py
@@ -0,0 +1,17 @@
+#Variables
+
+my_string_variable = " My Stryng variable"
+print(my_string_variable)
+
+my_int_variable = 5
+print(my_string_variable)
+
+my_bool_variable = False
+print(my_string_variable)
+
+
+
+
+
+#concatenacion de variables en un print
+print(my_string_variable,my_bool_variable , my_int_variable)
