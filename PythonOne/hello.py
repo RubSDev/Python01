191889379d543c5a9e8bf74e2806b06838a6a09d
Ruben Somuano <rubasomuano.g2@gmail.com>
2024-04-21 18:27:12 -0600

    fistr

---
+++ PythonOne/hello.py
@@ -0,0 +1,32 @@
+#Comentario en python
+#Hola mundo en python
+print ("Hola Python")
+
+"""
+Esto es un comentario 
+en varias lineas 
+en python
+"""
+
+
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+# Operaciones
+print(2 + 3)   # addition(+)
+print(3 - 1)   # subtraction(-)
+print(2 * 3)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# Checking data typescd
+# Consultar el tipo de dato
+print(type(10))                  # Int entero
+print(type(3.14))                # Float numerico
+print(type(1 + 3j))              # Complex numeros cpmplejos
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
